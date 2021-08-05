2e32051f22bc78c141b9b8280b5ec5021058399c
Guillaume Laforge <glaforge@gmail.com>
2021-08-06 01:44:07 +0200

    Receipt file chooser with thumbnails

--- public/js/request.js
+++ public/js/request.js
@@ -2,5 +2,28 @@ var app = new Vue({
     el: '#request',
     data: {
         reportId: window.location.pathname.substring(9).toUpperCase(),
+        images: []
+    },
+    methods: {
+        onFileChange: function(e) {
+            var files = e.target.files || e.dataTransfer.files;
+            if (!files.length) return;
+
+            this.images = [];
+            var vm = this;
+            Array.from(files).forEach((f, idx) => {
+                var reader = new FileReader();
+                reader.onload = (e) => {
+                    vm.images.push({
+                        src: e.target.result,
+                        title: f.name
+                    });
+                };
+                reader.readAsDataURL(f);
+            });
+        },
+        clickFileChooser: function(e) {
+            document.querySelector("#filesInput").click(e);
+        }
     }
 })
