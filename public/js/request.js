2621c83fb0f46544c64462dcc2469ad28c44068a
Guillaume Laforge <glaforge@gmail.com>
2021-08-06 02:07:21 +0200

    index not needed

--- public/js/request.js
+++ public/js/request.js
@@ -11,7 +11,7 @@ var app = new Vue({
 
             this.images = [];
             var vm = this;
-            Array.from(files).forEach((f, idx) => {
+            Array.from(files).forEach(f => {
                 var reader = new FileReader();
                 reader.onload = (e) => {
                     vm.images.push({
