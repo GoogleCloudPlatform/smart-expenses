3b9927ca507cc7f6c9ff73af4fc2c555e12ecfbe
Guillaume Laforge <glaforge@gmail.com>
2021-08-06 16:28:34 +0200

    First end-to-end call from web frontend to document-ai

--- public/js/request.js
+++ public/js/request.js
@@ -28,15 +28,47 @@ var app = new Vue({
     }
 })
 
-document.querySelector("#formFiles").addEventListener('sl-submit', event => {
-    // const formData = event.detail.formData;
-    // console.log("sl-submit", Array.from(formData.values()));
+document.querySelector("#formFiles").addEventListener('sl-submit', async (event) => {
     const reportId = app.reportId;
     const fileList = Array.from(document.querySelector("#filesInput").files);
 
+    // store in GCS via Firestore Storage
     const storageRef = firebase.storage().ref();
     fileList.forEach(f => {
         const receiptRef = storageRef.child(`${reportId}/${f.name}`);
         receiptRef.put(f).then(e => console.log("Uploaded", f.name));
     });
+    // TODO: ensure all files are uploaded before actually starting the call to the function workflow invoker
+
+    // call function to start workflow execution
+    const fileNames = fileList.map(f => f.name);
+    // const body = { reportId, fileNames };
+    const body = {
+        inputDocuments: {
+            gcsPrefix: {
+                gcsUriPrefix: `gs://smart-expenses-incoming-receipts/${reportId}`
+            }
+        },
+        documentOutputConfig: {
+            gcsOutputConfig: {
+                gcsUri: `gs://smart-expenses-parsed-receipts/${reportId}`
+            }
+        },
+        skipHumanReview: true
+    }
+
+    // TODO: avoid hard-coded function URL
+    const fnUrl = "https://europe-west1-easy-ai-serverless.cloudfunctions.net/invoke-workflow";
+    try {
+        const fnWorkflowResp = await fetch(fnUrl, {
+            method: "POST",
+            headers: { "Content-Type" : "application/json" },
+            body: JSON.stringify(body)
+        });
+        const outcome = await fnWorkflowResp.json();
+        console.log("Function workflow outcome", outcome);
+    } catch (e) {
+        // TODO: notify web UI in case of error
+        console.error(e);
+    }
 });
