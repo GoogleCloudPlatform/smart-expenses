7c7a33338dfffedca60c44ced53b6ff913bdcc91
Guillaume Laforge <glaforge@gmail.com>
2021-09-14 11:15:47 +0200

    Calling the callback URL via a function from the UI

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,5 @@
 Vue.use(Vuefire.firestorePlugin, { wait: true })
+
 const db = firebase.firestore();
 
 var app = new Vue({
@@ -8,5 +9,36 @@ var app = new Vue({
     },
     firestore: {
         reports: db.collection("requests").where("status", "==", "AWAITING")
+    },
+    methods: {
+        approve: function(reportId) {
+            console.log(`Approve ${reportId}`);
+            this.callbackCall(reportId, true);
+        },
+        reject: function(reportId) {
+            console.log(`Reject ${reportId}`)
+            this.callbackCall(reportId, false);
+        },
+        callbackCall: async function(reportId, approval) {
+            const report = this.reports.find(report => report.id == reportId);
+            
+            // TODO: avoid hard-coding the function URL
+            const callbackFunctionUrl = "https://europe-west1-easy-ai-serverless.cloudfunctions.net/approval-callback";
+            try {
+                const callbackResp = await fetch(callbackFunctionUrl, {
+                    method: "POST",
+                    headers: { "Content-Type" : "application/json" },
+                    body: JSON.stringify({ 
+                        url: report.callback,
+                        approved: approval 
+                    })
+                });
+                const outcome = await callbackResp.json();
+                console.log("Callback outcome", outcome);
+            } catch (e) {
+                // TODO: notify web UI in case of error
+                console.error(e);
+            }
+        }
     }
 })
